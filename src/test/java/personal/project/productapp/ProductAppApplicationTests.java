6cf647d2675904c2d8fb288079c87fbb68bba62a
Mohammed Faizan <156441568+im-faix@users.noreply.github.com>
2025-06-02 15:47:26 +0530

    Update ProductAppApplicationTests.java

--- src/test/java/personal/project/productapp/ProductAppApplicationTests.java
+++ src/test/java/personal/project/productapp/ProductAppApplicationTests.java
@@ -2,7 +2,7 @@ package personal.project.productapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+ 
 @SpringBootTest
 class ProductAppApplicationTests {
 
